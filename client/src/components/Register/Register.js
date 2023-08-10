0972dd906bc299e04cabdd2476b214be01118353
fafaew <rafael.gomest@gmail.com>
2023-08-10 19:20:15 -0300

    added api

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function register(e) {
+    e.preventDefault();
+    await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type': 'application/json'},
+    })
+  }
+
   return (
-    <form className='register'>
-      <input type='text' placeholder='username'/>
-      <input type='password' placeholder='password'/>
+    <form className='register' onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type='text'
+        placeholder='username'
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      />
+      <input
+        type='password'
+        placeholder='password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
       <button>Register</button>
     </form>
   )
