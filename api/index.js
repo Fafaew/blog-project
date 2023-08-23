5beeeb61839d3e4e69bb435e4937925faec79832
fafaew <rafael.gomest@gmail.com>
2023-08-23 19:56:06 -0300

    Login flow

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const app = express();
+const cookieParser = require('cookie-parser');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asiueuhaisehaiseh9sea';
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect("mongodb+srv://rafaelgomest:rafaelgomest@cluster0.lkz9qwo.mongodb.net/");
 
@@ -33,11 +35,26 @@ app.post('/login', async (req,res) => {
   if(passCorrect) {
     jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
       if(err) throw err;
-      res.cookie('token', token).json('ok');
+      res.cookie('token', token).json({
+        id:userDoc._id,
+        username,
+      });
     })
   } else {
-    res.status.json('wrong credentials')
+    res.status(400).json('wrong credentials')
   }
+})
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if(err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
 })
 
 app.listen(4000);
--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,20 @@ import HomePage from './components/Pages/HomePage';
 import Post from './components/Post/Post';
 import {Route, Routes} from "react-router-dom";
 import Register from './components/Register/Register';
+import UserContextProvider from './UserContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element = { <HomePage />} />
-        <Route path="/login" element = { <Login /> } />
-        <Route path="/register" element = { <Register /> } />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element = { <HomePage />} />
+          <Route path="/login" element = { <Login /> } />
+          <Route path="/register" element = { <Register /> } />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+
   );
 }
 
